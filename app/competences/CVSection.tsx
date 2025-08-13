a06c1bb2097f19e75c9c0b65ba49f5d56e4fcb7a
GGab1 <gabin.guerin1@gmail.com>
2025-08-13 17:24:29 +0200

    Fig bug v6

--- app/competences/CVSection.tsx
+++ app/competences/CVSection.tsx
@@ -1,20 +1,11 @@
-// CVSection.tsx
 'use client';
 
-import { useEffect, useState } from 'react';
+import CVViewer from '@/components/CVViewer';
 
 export default function CVSection() {
-    const [ViewerComp, setViewerComp] = useState<React.ReactNode>(null);
-
-    useEffect(() => {
-        import('@/components/CVViewer').then((mod) => {
-            setViewerComp(<mod.default />);
-        });
-    }, []);
-
     return (
-        <div className="w-full h-full overflow-hidden rounded-3xl bg-white">
-            {ViewerComp || <p className="text-center p-4 text-black">Chargement du CV en cours</p>}
+        <div className="w-full h-full overflow-hidden rounded-3xl bg-black">
+            <CVViewer />
         </div>
     );
 }
--- components/CVViewer.tsx
+++ components/CVViewer.tsx
@@ -3,22 +3,24 @@
 import dynamic from 'next/dynamic';
 
 const PDFViewer = dynamic(
-    () =>
-        import('@react-pdf-viewer/core').then(({ Worker, Viewer }) => {
-            return function Wrapper() {
-                return (
-                    <Worker workerUrl="https://unpkg.com/pdfjs-dist@3.11.174/build/pdf.worker.min.js">
-                        <Viewer fileUrl="/CV_Gabin-Guerin.pdf" />
-                    </Worker>
-                );
-            };
-        }),
+    async () => {
+        const mod = await import('@react-pdf-viewer/core');
+        const { Worker, Viewer } = mod;
+
+        return function Wrapper() {
+            return (
+                <Worker workerUrl="https://unpkg.com/pdfjs-dist@3.11.174/build/pdf.worker.min.js">
+                    <Viewer fileUrl="/CV_Gabin-Guerin.pdf" />
+                </Worker>
+            );
+        };
+    },
     { ssr: false }
 );
 
 export default function CVViewer() {
     return (
-        <div className="w-full h-full overflow-hidden rounded-3xl bg-white">
+        <div className="w-full h-full overflow-hidden rounded-3xl bg-black">
             <PDFViewer />
         </div>
     );
