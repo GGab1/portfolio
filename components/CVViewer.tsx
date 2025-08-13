35c521d215129fc4516492326357258c73cd1b0c
GGab1 <gabin.guerin1@gmail.com>
2025-08-13 17:35:16 +0200

    Fix bug v7

--- components/CVViewer.tsx
+++ components/CVViewer.tsx
@@ -1,27 +1,28 @@
 'use client';
-
-import dynamic from 'next/dynamic';
-
-const PDFViewer = dynamic(
-    async () => {
-        const mod = await import('@react-pdf-viewer/core');
-        const { Worker, Viewer } = mod;
-
-        return function Wrapper() {
-            return (
-                <Worker workerUrl="https://unpkg.com/pdfjs-dist@3.11.174/build/pdf.worker.min.js">
-                    <Viewer fileUrl="/CV_Gabin-Guerin.pdf" />
-                </Worker>
-            );
-        };
-    },
-    { ssr: false }
-);
+// import dynamic from 'next/dynamic';
+import { useEffect, useState } from 'react';
 
 export default function CVViewer() {
-    return (
-        <div className="w-full h-full overflow-hidden rounded-3xl bg-black">
-            <PDFViewer />
-        </div>
-    );
+  const [PDFWrapper, setPDFWrapper] = useState<React.FC | null>(null);
+
+  useEffect(() => {
+    (async () => {
+      const mod = await import('@react-pdf-viewer/core');
+      const { Worker, Viewer } = mod;
+
+      const Wrapper = () => (
+        <Worker workerUrl="https://unpkg.com/pdfjs-dist@3.11.174/build/pdf.worker.min.js">
+          <Viewer fileUrl="/CV_Gabin-Guerin.pdf" />
+        </Worker>
+      );
+
+      setPDFWrapper(() => Wrapper);
+    })();
+  }, []);
+
+  return (
+    <div className="w-full h-full overflow-hidden rounded-3xl bg-black">
+      {PDFWrapper ? <PDFWrapper /> : <p>Chargement du CV…</p>}
+    </div>
+  );
 }
