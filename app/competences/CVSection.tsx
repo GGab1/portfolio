50f86b55916ed11206928e62d8a3af86ec7c0cca
GGab1 <gabin.guerin1@gmail.com>
2025-08-13 11:13:35 +0200

    Fix probleme canva v2

--- app/competences/CVSection.tsx
+++ app/competences/CVSection.tsx
@@ -1,7 +1,19 @@
 'use client';
 
-import CVViewer from '@/components/CVViewer';
+import { useEffect, useState } from 'react';
 
 export default function CVSection() {
-    return <CVViewer />;
+    const [ViewerComp, setViewerComp] = useState<React.ReactNode>(null);
+
+    useEffect(() => {
+        import('@/components/CVViewer').then((mod) => {
+            setViewerComp(<mod.default />);
+        });
+    }, []);
+
+    return (
+        <div className="w-full h-full overflow-hidden rounded-3xl bg-black">
+            {ViewerComp || <p className="text-center p-4">Chargement du CV...</p>}
+        </div>
+    );
 }
